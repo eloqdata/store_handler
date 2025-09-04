22c1381130f5e889d3ebfa39a2563b43534427c9
githubzilla <githubzilla@gmail.com>
2025-09-04 11:28:56 +0800

    Add configurable branch_name

--- eloq_data_store_service/rocksdb_cloud_data_store.cpp
+++ eloq_data_store_service/rocksdb_cloud_data_store.cpp
@@ -385,7 +385,6 @@ bool RocksDBCloudDataStore::StartDB()
         LOG(ERROR) << "Failed to find max term from cloud manifest file for "
                       "branch: "
                    << branch_name_;
-        return false;
     }
 
     if (max_term != -1)
@@ -396,6 +395,7 @@ bool RocksDBCloudDataStore::StartDB()
     }
     else
     {
+        // this is a new db
         cookie_on_open = "";
         new_cookie_on_open = MakeCloudManifestCookie(branch_name_, ng_id, 0);
     }
